0c1c4c7e9b86581336b26e4afd583ca628b4ba46
manishraghavendra <www.manishrag@gmail.com>
2024-07-01 08:32:10 +0530

    fixed some known issues

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,7 +1,7 @@
 function Textarea({ value, onChange, disabled, placeholder, onFocus }) {
   return (
     <textarea
-      className="w-full resize-none p-3 border-2 shadow-custom my-2 rounded-e-lg rounded-b-lg max-h-16 text-mb outline-none font-normal bg-gray-950 text-blue-100"
+      className="w-full resize-none p-3 border-2 shadow-custom my-2 rounded-e-lg rounded-b-lg max-h-fit text-mb outline-none font-normal bg-gray-950 text-blue-100"
       maxLength={1001}
       value={value}
       onChange={onChange}
